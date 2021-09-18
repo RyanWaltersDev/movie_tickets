db7dd24e6efcf693e8624357d24a3abd7dc2d055
Ryan Walters <RyanWaltersDev@gmail.com>
2021-09-18 08:07:23 -0400

    +broken price calculator test

--- price_calculator_2.py
+++ price_calculator_2.py
@@ -1,23 +1,24 @@
-class PriceCalculator():
+class PriceCalculator:
     '''Calculate ticket price.'''
 
-    def __init__(self, age, ticket=0, ticket_list=[]):
+    def __init__(self, age=0, ticket=0, ticket_list=[]):
         '''Initialize attributes.'''
         self.age = age
         self.ticket = ticket
         self.ticket_list = ticket_list
 
-    def younger_than_3(self, age):
+    def younger_than_3(self, age, ticket=0, ticket_list=[]):
         '''Print message and add no price.'''
-        return f"Your {self.age} year old child gets free admission!"
+        return (ticket, ticket_list, 
+            f"Your {age} year old child gets free admission!")
 
     def ages_3_to_12(self, age, ticket=0, ticket_list=[]):
         '''Return ticket price and message for ages 3 to 12.'''
         ticket += 10
         ticket += ticket * 0.07
         ticket_list.append(ticket)
-        return (ticket, ticket_list, 
-            f"The ticket price for a {self.age} year old is $10!")
+        return [ticket, ticket_list, 
+            f"The ticket price for a {age} year old is $10!"]
 
     def ages_13_to_64(self, age, ticket=0, ticket_list=[]):
         '''Return ticket price and message for ages 13 to 64.'''
@@ -25,7 +26,7 @@ class PriceCalculator():
         ticket += ticket * 0.07
         ticket_list.append(ticket)
         return(ticket, ticket_list,
-            f"The ticket price for {self.age} year olds is $15!")
+            f"The ticket price for {age} year olds is $15!")
 
     def ages_65_and_older(self, age, ticket=0, ticket_list=[]):
         '''Return ticket price and message for ages 65 and up.'''
@@ -35,3 +36,26 @@ class PriceCalculator():
         return(ticket, ticket_list,
             f"The ticket price for senior citizens is $12!")
 
+    def input_logic(self, age):
+        '''Return function based on age input'''
+        age = int(age)
+        if age < 3:
+            return self.younger_than_3(age)
+        elif age >= 3 and age <= 12:
+            return self.ages_3_to_12(age)
+        elif age > 12 and age < 65:
+            return self.ages_13_to_64(age)
+        elif age >= 65 and age <=120:
+            return self.ages_65_and_older(age)
+        else:
+            return (0, [], "Please enter a valid age.")
+
+#instance = PriceCalculator()
+#customer = instance.ages_3_to_12(12)
+#print(customer)
+
+#instance = PriceCalculator()
+#a,b,c = instance.input_logic(12)
+#print(a)
+#print(b)
+#print(c)
--- price_calculator_test.py
+++ price_calculator_test.py
@@ -4,35 +4,47 @@ from price_calculator_2 import PriceCalculator
 
 class PriceCalculatorTest(unittest.TestCase):
     '''Make sure our price calculator works'''
+    def setUp(self):
+        self.instance = PriceCalculator()
+
+    def tearDown(self):
+        self.instance.ticket_list = []
 
     def test_younger_than_3(self):
         '''Check to make sure that function prints correctly.'''
-        age = PriceCalculator(2)
-        customer = age.younger_than_3(2)
-        self.assertEqual(customer, 'Your 2 year old child '
-            'gets free admission!')
+        instance = PriceCalculator()
+        customer = instance.younger_than_3(2)
+        self.assertEqual(customer, (0, [], 'Your 2 year old child '
+            'gets free admission!'))
 
     def test_ages_3_to_12(self):
         '''Check funtion formula and return message.'''
-        age = PriceCalculator(5)
-        customer = age.ages_3_to_12(4)
-        self.assertEqual(customer, (10.7, [10.7], 
-            "The ticket price for a 5 year old is $10!"))
+        instance = PriceCalculator()
+        ticket, ticket_list, string = instance.input_logic(12)
+        self.assertEqual(ticket, 10.7)
+        self.assertEqual(ticket_list, [10.7])
+        self.assertEqual(string, "The ticket price for a 12 year old is $10!")
 
     def test_ages_13_to_64(self):
         '''Check function formula and return message.'''
-        age = PriceCalculator(25)
-        customer = age.ages_13_to_64(25)
+        instance = PriceCalculator()
+        customer = instance.ages_13_to_64(25)
         self.assertEqual(customer, (16.05, [16.05], 
             "The ticket price for 25 year olds is $15!"))
 
     def test_ages_65_and_older(self):
         '''Check function formula and return message.'''
-        age = PriceCalculator(76)
-        customer = age.ages_65_and_older(76)
+        instance = PriceCalculator()
+        customer = instance.ages_65_and_older(76)
         self.assertEqual(customer, (12.84, [12.84],
             "The ticket price for senior citizens is $12!"))
 
+    def test_input_logic(self):
+        instance = PriceCalculator()
+        ticket, ticket_list, string = instance.input_logic(12)
+        self.assertEqual(ticket, 10.7)
+        self.assertEqual(ticket_list, [10.7])
+        self.assertEqual(string, "The ticket price for a 12 year old is $10!")
 
 if __name__ == '__main__':
     unittest.main()
